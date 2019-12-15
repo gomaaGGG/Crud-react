6b8749e0b785dcd93c2dddf43ee835be2823ba5d
gomaa elmokhtar <gomaamok44@gmail.com>
2019-12-15 23:10:11 +0200

    Updates

---
+++ precache-manifest.2ff78463a32be40dddb8e64cddaed414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a472bf3661eb7edb37994cf3cb5910",
+    "url": "/Crud-react/index.html"
+  },
+  {
+    "revision": "6393aac657768dc72b63",
+    "url": "/Crud-react/static/css/main.64bddb6c.chunk.css"
+  },
+  {
+    "revision": "b362ecc8df4777869f1e",
+    "url": "/Crud-react/static/js/2.060b6a8a.chunk.js"
+  },
+  {
+    "revision": "6393aac657768dc72b63",
+    "url": "/Crud-react/static/js/main.5c429869.chunk.js"
+  },
+  {
+    "revision": "07fb7276bc5345ce1a60",
+    "url": "/Crud-react/static/js/runtime-main.af29e116.js"
+  }
+]);
